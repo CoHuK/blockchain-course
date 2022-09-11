19e1b7e1acaf116c3c89fea85b60239250ec14ae
CoHuK <cohuk2002@gmail.com>
2022-09-11 14:39:38 +0800

    4 more lessons

--- contracts/Mappings.sol
+++ contracts/Mappings.sol
@@ -1,10 +1,15 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.16;
+pragma solidity ^0.8.17;
 
 contract Mappings {
     mapping(uint => bool) public myMapping;
+    mapping(address => bool) public myAddressMapping;
 
     function setMapping (uint index) public {
         myMapping[index] = true;
+    }
+
+    function setAddressMapping() public {
+        myAddressMapping[msg.sender] = true;
     }
 }
